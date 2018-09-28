a03fb437fba9c05b2155eb2ec68bde0c7d3f5ea6
Sung Wung, Yeom <yeom4032yeom4032@gmail.com>
2018-09-28 10:51:42 +0900

    Update test.js

--- TestAPI/test.js
+++ TestAPI/test.js
@@ -6,7 +6,7 @@ rtspUrl.push('rtsp://192.168.1.106:8555/unicast');
 
 var host = '192.168.1.104';
 var url_ = rtspUrl[0];
-var ports = 8081;  //  web port  - ports[0]
+var ports = 8080; //  web port  - ports[0]
 var cameraName = 'Test';
 var callback = '';
 
