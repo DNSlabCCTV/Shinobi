968bbc52a1e5aba3e833e80864c3a31cebaf761f
Sung Wung, Yeom <yeom4032yeom4032@gmail.com>
2018-09-28 10:50:45 +0900

    Update test.js

--- TestAPI/test.js
+++ TestAPI/test.js
@@ -1,21 +1,23 @@
-var Shinobi = require(__dirname + "/index");
-
+var Shinobi = require(__dirname + "/Module");
 var rtspUrl = [];
+
+// write RTSP URL
 rtspUrl.push('rtsp://192.168.1.106:8555/unicast');
-rtspUrl.push('rtsp://admin:dns5303438@192.168.1.102:88/videoMain');
 
 var host = '192.168.1.104';
-var url_ = rtspUrl[0]; //  rtsp://admin:dns3438@192.168.1.107:88/videoMain
+var url_ = rtspUrl[0];
 var ports = 8081;  //  web port  - ports[0]
 var cameraName = 'Test';
 var callback = '';
 
+// Set up Camera
 Shinobi.ShinobiSetup(host, ports, cameraName, rtspUrl, callback, function(result,data){
         if (result){
             console.log(result);
         }
 });
 
+// Delete Camera
 /*
 Shinobi.ShinobiDeleteMonitor(host, ports, cameraName, callback,function(result,data){
         if (result){
